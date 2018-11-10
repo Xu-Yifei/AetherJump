afc8984bd6a8c367d6554c9a33a56ba918596eb5
Yifei Xu <yifei21xu@gmail.com>
2018-11-10 15:15:37 -0500

    Add actual game files

---
+++ First Person Map Test/Assets/PlayerMove.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+
+    [SerializeField] private float walkSpeed,sprintSpeed;
+    [SerializeField] private float sprintBuildUp;
+    [SerializeField] private KeyCode sprintKey;
+
+    private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        SetSpeed();
+        JumpInput();
+    }
+
+    private void SetSpeed()
+    {
+        if (Input.GetKey(sprintKey))
+        {
+            movementSpeed = Mathf.Lerp(movementSpeed, sprintSpeed, Time.deltaTime * sprintBuildUp);
+        }
+        else
+        {
+            movementSpeed = Mathf.Lerp(movementSpeed, walkSpeed, Time.deltaTime * sprintBuildUp);
+        }
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping && charController.isGrounded)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
